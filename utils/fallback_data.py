59605499e6e4c00525cc8ed7ea9299f72708c23b
kuochili-ops <kuochili@gmail.com>
2025-11-20 10:12:19 +0800

    Create fallback_data.py

---
+++ utils/fallback_data.py
@@ -0,0 +1,32 @@
+fda_list = [
+    {
+        "alert_date": "2025-11-01",
+        "source": "DSC",
+        "us_product": "Leqembi",
+        "ingredient": "lecanemab",
+        "form": "100 mg/mL 注射液",
+        "risk_summary": "阿茲海默症 ARIA：APOE ε4 攜帶者風險增加",
+        "action_summary": "建議基因檢測",
+        "fda_excerpt": "FDA recommends MRI monitoring to reduce ARIA risk, especially in APOE ε4 carriers."
+    },
+    {
+        "alert_date": "2025-10-15",
+        "source": "DSC",
+        "us_product": "Prolia",
+        "ingredient": "denosumab",
+        "form": "60 mg/1 mL 注射液",
+        "risk_summary": "嚴重低血鈣：洗腎病人風險增加",
+        "action_summary": "建議監測血鈣",
+        "fda_excerpt": "Risk of severe hypocalcemia in dialysis patients receiving denosumab."
+    },
+    {
+        "alert_date": "2025-09-30",
+        "source": "DSC",
+        "us_product": "Ocaliva",
+        "ingredient": "obeticholic acid",
+        "form": "5 mg 錠劑",
+        "risk_summary": "原發性膽汁性肝硬化：晚期肝病病人風險增加",
+        "action_summary": "建議調整劑量",
+        "fda_excerpt": "Serious liver injury reported in non-cirrhotic PBC patients treated with obeticholic acid."
+    }
+]
